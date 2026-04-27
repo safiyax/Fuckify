e7e21dd597472d29a5d39d9165185f3487ead4b4
safiyax <safiya@sparselabs.org>
2026-04-27 12:14:01 -0600

    feat: add Premium row to ExperimentsView

--- Fuckify/Features/Experiments/ExperimentsView.swift
+++ Fuckify/Features/Experiments/ExperimentsView.swift
@@ -6,18 +6,37 @@ import SwiftUI
 
 struct ExperimentsView: View {
     @Environment(FeatureFlagsProvider.self) private var featureFlags
+    @Environment(PremiumManager.self) private var premium
     @State private var registeredUsername = UserDefaults.standard.string(forKey: "cc.username")
+    @State private var showManagement = false
 
     var body: some View {
         Group {
-            if featureFlags.settings.more.experimentsFlags.userRegistration {
+            if featureFlags.settings.more.experimentsFlags.userRegistration ||
+               featureFlags.settings.more.experimentsFlags.paywall {
                 Form {
-                    Section {
-                        if let username = registeredUsername {
-                            LabeledContent("User Registration", value: "Registered as @\(username)")
-                        } else {
-                            NavigationLink("User Registration") {
-                                RegistrationContainerView()
+                    if featureFlags.settings.more.experimentsFlags.userRegistration {
+                        Section {
+                            if let username = registeredUsername {
+                                LabeledContent("User Registration", value: "Registered as @\(username)")
+                            } else {
+                                NavigationLink("User Registration") {
+                                    RegistrationContainerView()
+                                }
+                            }
+                        }
+                    }
+
+                    if featureFlags.settings.more.experimentsFlags.paywall {
+                        Section {
+                            if premium.isPremium {
+                                LabeledContent("Premium", value: "Active")
+                                    .contentShape(Rectangle())
+                                    .onTapGesture { showManagement = true }
+                            } else {
+                                NavigationLink("Premium") {
+                                    PaywallView()
+                                }
                             }
                         }
                     }
@@ -30,6 +49,13 @@ struct ExperimentsView: View {
         .onAppear {
             registeredUsername = UserDefaults.standard.string(forKey: "cc.username")
         }
+        .task {
+            await premium.load()
+        }
+        .sheet(isPresented: $showManagement) {
+            PremiumManagementView()
+                .environment(premium)
+        }
     }
 }
 
@@ -37,5 +63,6 @@ struct ExperimentsView: View {
     NavigationStack {
         ExperimentsView()
             .environment(FeatureFlagsProvider())
+            .environment(PremiumManager.shared)
     }
 }
